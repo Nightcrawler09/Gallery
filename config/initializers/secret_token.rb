ce5ad136ed955ae9521866f411f238bcfe149617
Fran <fran@fran-K53SJ.(none)>
2012-09-14 12:14:33 +0100

    first commit of the proyect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galery::Application.config.secret_token = 'f55a7639a4daf0eb0e779cd2a2f20b99151d2824ee568e92f181b630b36e94bf944c58ce13234dc34b39e4e9f86045249189a15408b6b11a8a75e01d1295ba6f'
